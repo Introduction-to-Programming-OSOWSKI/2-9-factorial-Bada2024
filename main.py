c376f305e85d5e9dc0f09acb926b4a369b10b58f
Bada2024 <830561@mystma.org>
2021-10-05 09:09:42 -0500

    1st

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 #WRITE YOUR CODE IN THIS FILE
+def factorial(x):
+    # define the function with 1 parameter
+    y=1
+    #create variable to represnt the number that will
+    for i in range(1,x):
+        y= y * i
+    return y
+print(factorial())
+        
